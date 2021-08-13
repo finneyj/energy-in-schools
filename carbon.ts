7fbd215b49c027b9a9262541e84b16059c986341
Joe Finney <joe@comp.lancs.ac.uk>
2021-08-13 15:02:34 +0100

    Remove banana code, add energy in schools TS files

---
+++ carbon.ts
@@ -0,0 +1,108 @@
+/**
+ * Get information about carbon emission level for different energy types
+ */
+//% color=#004d00 weight=90 icon="\uf3fd" block="Carbon"
+namespace carbon {
+
+    /**
+     * Sets carbon index value type
+     * @param index target carbon index type; eg: CarbonIndex.moderate
+     */
+    //% help=carbon/set-carbon-index-value-type
+    //% weight=100
+    //% blockId=carbon_setCarbonIndexValueType
+    //% block="carbon index level %index"
+    //% shim=carbon::setCarbonIndexValueType
+    export function setCarbonIndexValueType(index:CarbonIndex): number {
+        return index;
+    }
+
+    /**
+     * Get current carbon emission value
+     */
+    //% help=carbon/query-carbon-value
+    //% weight=99
+    //% blockId=carbon_queryCarbonValue
+    //% block="get carbon value"
+    //% shim=carbon::queryCarbonValue
+    export function queryCarbonValue(): number {
+        return Math.floor(Math.random() * 25) + 100;
+    }
+
+    /**
+     * Get current carbon emission index level
+     */
+    //% help=carbon/query-carbon-index
+    //% weight=98
+    //% blockId=carbon_queryCarbonIndex
+    //% block="get carbon index level"
+    //% shim=carbon::queryCarbonIndex
+    export function queryCarbonIndex(): number {
+        return Math.floor(Math.random() * 25) + 100;
+    }
+
+    /**
+     * Get current carbon emission index level as text
+     */
+    //% help=carbon/query-carbon-index-text
+    //% weight=97
+    //% blockId=carbon_queryCarbonIndexText
+    //% block="get carbon index level as text"
+    //% shim=carbon::queryCarbonIndexText
+    export function queryCarbonIndexText(): string {
+        let n = Math.floor(Math.random() * 5);
+        
+        switch(n)
+        {
+            case 0:
+                return "very low";
+            case 1:
+                return "low";
+            case 2:
+                return "moderate";
+            case 3:
+                return "high";
+            case 4:
+                return "very high";
+        }
+        return "moderate";
+    }
+
+    /**
+     * Get current carbon generation mix for specific type
+     * @param mixType mixType; eg: GenerationMixType.gas
+     */
+    //% help=carbon/query-carbon-generation-mix
+    //% weight=96
+    //% blockId=carbon_queryCarbonGenerationMix
+    //% block="get carbon generation mix|for type %mix"
+    //% shim=carbon::queryCarbonGenerationMix
+    export function queryCarbonGenerationMix(mixType : GenerationMixType): number
+    {
+        if(mixType == GenerationMixType.biomas){
+            return Math.floor(Math.random() * 10) + 5;
+        }
+        if(mixType == GenerationMixType.coal){
+            return Math.floor(Math.random() * 10) + 15;
+        }
+        if(mixType == GenerationMixType.imports){
+            return Math.floor(Math.random() * 10) + 5;
+        }
+        if(mixType == GenerationMixType.gas){
+            return Math.floor(Math.random() * 10) + 20;
+        }
+        if(mixType == GenerationMixType.nuclear){
+            return Math.floor(Math.random() * 10) + 10;
+        }
+        if(mixType == GenerationMixType.hydro){
+            return Math.floor(Math.random() * 10) + 1;
+        }
+        if(mixType == GenerationMixType.solar){
+            return Math.floor(Math.random() * 10) + 10;
+        }
+        if(mixType == GenerationMixType.wind){
+            return Math.floor(Math.random() * 10) + 2;
+        }
+        return 0;
+    }
+}
---
+++ weather.ts
@@ -0,0 +1,66 @@
+//% color=243 weight=100 icon="\uf185" block="Weather"
+namespace weather {
+
+    /**
+      * Get current temperature in city or by location postcode
+      * @param weatherLocationType city or postcode; eg: WeatherLocationType.city
+      * @param location city name or location postcode; eg: "London"
+      */
+    //% help=weather/get-temperature
+    //% weight=96
+    //% blockId=get_place_temp
+    //% block="get|temperature|for %weatherLocationType | %location" blockGap=8
+    //% inlineInputMode=inline
+    //% shim=weather::getTemperature
+    export function getTemperature(weatherLocationType: WeatherLocationType, location: string) : string
+    {
+        return Math.floor(Math.random() * 25).toString();
+    }
+
+    /**
+      * Get current wind direction in city or by location postcode
+      * @param weatherLocationType city or postcode; eg: WeatherLocationType.city
+      * @param location city name or location postcode; eg: "London"
+      */
+    //% help=weather/get-wind-direction
+    //% weight=96
+    //% blockId=get_wind_dir
+    //% block="get|wind direction for %weatherLocationType | %location" blockGap=8
+    //% shim=weather::getWindDirection
+    export function getWindDirection(weatherLocationType: WeatherLocationType, location: string) : string
+    {
+        let directions = ["N", "NW", "NE","S","SE","SW","E","W"]
+        return directions[Math.floor(Math.random() * 7)]
+    }
+
+    /**
+      * Get current weather forecast for city or by location postcode
+      * @param weatherLocationType city or postcode; eg: WeatherLocationType.city
+      * @param location city name or location postcode; eg: "London"
+      */
+    //% help=weather/get-weather-forecast
+    //% weight=96
+    //% blockId=get_forecast_now
+    //% block="get|weather forecast for %weatherLocationType | %location" blockGap=8
+    //% shim=weather::getWeatherForecast
+    export function getWeatherForecast(weatherLocationType: WeatherLocationType, location: string) : string
+    {
+        let weather = ["overcast", "raining", "sunny", "snowing", "thunder"]
+        return weather[Math.floor(Math.random() * 4)]
+    }
+
+    /**
+      * Get tomorrows weather forecast for city or by location postcode
+      * @param weatherLocationType city or postcode; eg: WeatherLocationType.city
+      * @param location city name or location postcode; eg: "London"
+      */
+    //% help=weather/get-weather-forecast-tomorrow
+    //% weight=96
+    //% blockId=get_forecast_tom block="get|tomorrows weather forecast for %weatherLocationType | %location" blockGap=8
+    //% shim=weather::getWeatherForecastTomorrow
+    export function getWeatherForecastTomorrow(weatherLocationType: WeatherLocationType, location: string)
+    {
+        let weather = ["overcast", "raining", "sunny", "snowing", "thunder"]
+        return weather[Math.floor(Math.random() * 4)]
+    }
+}
