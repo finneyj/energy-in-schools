8a1c22d50c54fd06e73c9ebea66e330ad29dad85
Joe Finney <joe@comp.lancs.ac.uk>
2021-08-16 17:44:24 +0100

    Simplify SchoolEnergy blocks

--- energy.cpp
+++ energy.cpp
@@ -8,70 +8,14 @@ enum class EnergyType{
     solar = 2,
 };
 
-enum class HourType{
-    _1_hour_ago = 1,
-    _2_hours_ago = 2,
-    _3_hours_ago = 3,
-    _4_hours_ago = 4,
-    _5_hours_ago = 5,
-    _6_hours_ago = 6,
-    _7_hours_ago = 7,
-    _8_hours_ago = 8,
-    _9_hours_ago = 9,
-    _10_hours_ago = 10,
-    _11_hours_ago = 11,
-    _12_hours_ago = 12,
-    _13_hours_ago = 13,
-    _14_hours_ago = 14,
-    _15_hours_ago = 15,
-    _16_hours_ago = 16,
-    _17_hours_ago = 17,
-    _18_hours_ago = 18,
-    _19_hours_ago = 19,
-    _20_hours_ago = 20,
-    _21_hours_ago = 21,
-    _22_hours_ago = 22,
-    _23_hours_ago = 23,
-    _24_hours_ago = 24,
-    _25_hours_ago = 25,
+enum class TimeType{
+    hours = 0,
+    days = 1,
+    weeks = 2,
+    months = 3
 };
 
-enum class DayType{
-    yesterday = 1,
-    _2_days_ago = 2,
-    _3_days_ago = 3,
-    _4_days_ago = 4,
-    _5_days_ago = 5,
-    _6_days_ago = 6,
-    _7_days_ago = 7,
-    _8_days_ago = 8,
-};
-
-enum class WeekType{
-    last_week = 1,
-    _2_weeks_ago = 2,
-    _3_weeks_ago = 3,
-    _4_weeks_ago = 4,
-    _5_weeks_ago = 5,
-};
-
-enum class MonthType{
-    last_month = 1,
-    _2_months_ago = 2,
-    _3_months_ago = 3,
-    _4_months_ago = 4,
-    _5_months_ago = 5,
-    _6_months_ago = 6,
-    _7_months_ago = 7,
-    _8_months_ago = 8,
-    _9_months_ago = 9,
-    _10_months_ago = 10,
-    _11_months_ago = 11,
-    _12_months_ago = 12,
-    _13_months_ago = 13,
-};
-
-
+const char *TimeTypeNames[] = {"hour", "day", "week","month"};
 
 /**
 * share
@@ -187,58 +131,6 @@ namespace energy {
         return reply.leakData();
     }
 
-    //%
-    int queryEnergy(EnergyType energyType, String schoolId)
-    {
-        ManagedString result = queryEnergyText(energyType,MSTR(schoolId),"");
-        int value = stringToNumber(result);
-        return value;
-    }
-
-
-    //%
-    int queryEnergyHour(EnergyType energyType, HourType hourType, String schoolId)
-    {
-        char buffer[20];
-        memset(buffer,0,sizeof(buffer));
-        sprintf(buffer,"hour/%d", (int)hourType);
-        ManagedString result = queryEnergyText(energyType, MSTR(schoolId), ManagedString(buffer));
-        int value = stringToNumber(result);
-        return value;
-    }
-
-    //%
-    int queryEnergyDay(EnergyType energyType, DayType dayType, String schoolId)
-    {
-        char buffer[20];
-        memset(buffer,0,sizeof(buffer));
-        sprintf(buffer,"day/%d", (int)dayType);
-        ManagedString result = queryEnergyText(energyType, MSTR(schoolId), ManagedString(buffer));
-        int value = stringToNumber(result);
-        return value;
-    }
-
-    //%
-    int queryEnergyWeek(EnergyType energyType, WeekType weekType, String schoolId)
-    {
-        char buffer[20];
-        memset(buffer,0,sizeof(buffer));
-        sprintf(buffer,"week/%d", (int)weekType);
-        ManagedString result = queryEnergyText(energyType, MSTR(schoolId), ManagedString(buffer));
-        int value = stringToNumber(result);
-        return value;
-    }
-
-    //%
-    int queryEnergyMonth(EnergyType energyType, MonthType monthType, String schoolId)
-    {
-        char buffer[20];
-        memset(buffer,0,sizeof(buffer));
-        sprintf(buffer,"month/%d", (int)monthType);
-        ManagedString result = queryEnergyText(energyType, MSTR(schoolId), ManagedString(buffer));
-        int value = stringToNumber(result);
-        return value;
-    }
 
     //%
     int querySchoolEnergy(EnergyType energyType)
@@ -248,50 +140,24 @@ namespace energy {
         return value;
     }
 
+    //%
+    int querySchoolEnergyFor(EnergyType energyType, String schoolId)
+    {
+        ManagedString result = queryEnergyText(energyType,MSTR(schoolId),"");
+        int value = stringToNumber(result);
+        return value;
 
+    }
 
     //%
-    int querySchoolEnergyDay(EnergyType energyType, DayType dayType)
+    int querySchoolEnergyAgo(EnergyType energyType, int units, TimeType period)
     {
         char buffer[20];
         memset(buffer,0,sizeof(buffer));
-        sprintf(buffer,"day/%d", (int)dayType);
+        sprintf(buffer,"%s/%d", TimeTypeNames[(int)period], units);
+
         ManagedString result = queryEnergyText(energyType, ManagedString("local"), ManagedString(buffer));
         int value = stringToNumber(result);
         return value;
     }
-
-    //%
-    int querySchoolEnergyHour(EnergyType energyType, HourType hourType)
-    {
-        char buffer[20];
-        memset(buffer,0,sizeof(buffer));
-        sprintf(buffer,"hour/%d", (int)hourType);
-        ManagedString result = queryEnergyText(energyType, ManagedString("local"), ManagedString(buffer));
-        int value = stringToNumber(result);
-        return value;
-    }
-
-    //%
-    int querySchoolEnergyWeek(EnergyType energyType, WeekType weekType)
-    {
-        char buffer[20];
-        memset(buffer,0,sizeof(buffer));
-        sprintf(buffer,"week/%d", (int)weekType);
-        ManagedString result = queryEnergyText(energyType, ManagedString("local"), ManagedString(buffer));
-        int value = stringToNumber(result);
-        return value;
-    }
-
-    //%
-    int querySchoolEnergyMonth(EnergyType energyType, MonthType monthType)
-    {
-        char buffer[20];
-        memset(buffer,0,sizeof(buffer));
-        sprintf(buffer,"month/%d", (int)monthType);
-        ManagedString result = queryEnergyText(energyType, ManagedString("local"), ManagedString(buffer));
-        int value = stringToNumber(result);
-        return value;
-    }
-
 };
--- energy.ts
+++ energy.ts
@@ -4,149 +4,27 @@
 //% color=#006f00 weight=100 icon="\uf275" block="School Energy"
 namespace energy {
 
-
-    /**
-     * Get selected energy type current usage for my school
-     * @param energyType energy type; eg: EnergyType.electricity
-     */
-    //% help=energy/get-my-school-energy-usage
-    //% weight=100
+    //% weight=98
     //% blockId=energy_querySchoolEnergy
-    //% block="get energy level|for %energyType |for my school"
+    //% block="get my school| %energyType usage"
     //% shim=energy::querySchoolEnergy
     export function querySchoolEnergy(energyType: EnergyType): number {
         return Math.floor(Math.random() * 25) + 100;
     }
 
-    /**
-     * Get my school selected energy type total usage for selected time in hours
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param hourType time amount; eg: HourType._1_hour_ago
-     */
-    //% help=energy/get-my-school-energy-usage-for-hours
-    //% weight=98
-    //% blockId=energy_querySchoolEnergyHour
-    //% block="get energy level|for %energyType |for hour %hourType|for my school"
-    //% shim=energy::querySchoolEnergyHour
-    export function querySchoolEnergyHour(energyType: EnergyType, hourType: HourType): number {
-        return Math.floor(Math.random() * 25) + 100;
-    }
-
-    /**
-     * Get my school selected energy type total usage for selected time in days
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param dayType time amount; eg: DayType.yesterday
-     */
-    //% help=energy/get-my-school-energy-usage-for-days
-    //% weight=96
-    //% blockId=energy_querySchoolEnergyDay
-    //% block="get energy level|for %energyType |for day %dayType|for my school"
-    //% shim=energy::querySchoolEnergyDay
-    export function querySchoolEnergyDay(energyType: EnergyType, dayType: DayType): number {
-        return Math.floor(Math.random() * 25) + 100;
-    }
-
-    /**
-     * Get my school selected energy type total usage for selected time in weeks
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param weekType time amount; eg: WeekType.last_week
-     */
-    //% help=energy/get-my-school-energy-usage-for-weeks
-    //% weight=94
-    //% blockId=energy_querySchoolEnergyWeek
-    //% block="get energy level|for %energyType |for week %weekType|for my school"
-    //% shim=energy::querySchoolEnergyWeek
-    export function querySchoolEnergyWeek(energyType: EnergyType, weekType: WeekType): number {
-        return Math.floor(Math.random() * 25) + 100;
-    }
-
-
-    /**
-     * Get my school selected energy type total usage for selected time in months
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param monthType time amount; eg: MonthType.last_month
-     */
-    //% help=energy/get-my-school-energy-usage-for-months
-    //% weight=92
-    //% blockId=energy_querySchoolEnergyMonth
-    //% block="get energy level|for %energyType |for month %monthType|for my school"
-    //% shim=energy::querySchoolEnergyMonth
-    export function querySchoolEnergyMonth(energyType: EnergyType, monthType: MonthType): number {
-        return Math.floor(Math.random() * 25) + 100;
-    }
-
-    /**
-     * Get selected energy type current usage for other school
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param schoolId target school ID; eg: AB123
-     */
-    //% help=energy/get-other-school-energy-usage
-    //% weight=99
-    //% blockId=energy_queryEnergy
-    //% block="get energy level|for %energyType |for other school %schoolId"
-    //% shim=energy::queryEnergy
-    export function queryEnergy(energyType: EnergyType, schoolId: string): number {
-        return Math.floor(Math.random() * 25) + 100;
-    }
-
-
-    /**
-     * Get other school selected energy type total usage for selected time in hours
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param hourType time amount; eg: HourType._1_hour_ago
-     * @param schoolId target school ID; eg: AB123
-     */
-    //% help=energy/get-other-school-energy-usage-for-hours
     //% weight=97
-    //% blockId=energy_queryEnergyHour
-    //% block="get energy level|for %energyType |for hour %hourType |for other school %schoolId"
-    //% shim=energy::queryEnergyHour
-    export function queryEnergyHour(energyType: EnergyType, hourType: HourType, schoolId: string): number {
+    //% blockId=energy_querySchoolEnergyPeriod
+    //% block="get my school| %energyType | usage for %units | %period ago"
+    //% shim=energy::querySchoolEnergyAgo
+    export function querySchoolHistoricEnergy(energyType: EnergyType, units: number, period: TimeType): number {
         return Math.floor(Math.random() * 25) + 100;
     }
 
-    /**
-     * Get other school selected energy type total usage for selected time in days
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param dayType time amount; eg: DayType.yesterday
-     * @param schoolId target school ID; eg: AB123
-     */
-    //% help=energy/get-other-school-energy-usage-for-days
-    //% weight=95
-    //% blockId=energy_queryEnergyDay
-    //% block="get energy level|for %energyType |for day %dayType |for other school %schoolId"
-    //% shim=energy::queryEnergyDay
-    export function queryEnergyDay(energyType: EnergyType, dayType: DayType, schoolId: string): number {
-        return Math.floor(Math.random() * 25) + 100;
-    }
-
-    /**
-     * Get other school selected energy type total usage for selected time in weeks
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param weekType time amount; eg: WeekType.last_week
-     * @param schoolId target school ID; eg: AB123
-     */
-    //% help=energy/get-other-school-energy-usage-for-weeks
-    //% weight=93
-    //% blockId=energy_queryEnergyWeek
-    //% block="get energy level|for %energyType |for week %weekType |for other school %schoolId"
-    //% shim=energy::queryEnergyWeek
-    export function queryEnergyWeek(energyType: EnergyType, weekType: WeekType, schoolId: string): number {
-        return Math.floor(Math.random() * 25) + 100;
-    }
-
-    /**
-     * Get other school selected energy type total usage for selected time in months
-     * @param energyType energy type; eg: EnergyType.electricity
-     * @param monthType time amount; eg: MonthType.last_months
-     * @param schoolId target school ID; eg: AB123
-     */
-    //% help=energy/get-other-school-energy-usage-for-months
-    //% weight=91
-    //% blockId=energy_queryEnergyMonth
-    //% block="get energy level|for %energyType |for month %monthType |for other school %schoolId"
-    //% shim=energy::queryEnergyMonth
-    export function queryEnergyMonth(energyType: EnergyType, monthType: MonthType, schoolId: string): number {
+    //% weight=96
+    //% blockId=energy_queryOtherSchool
+    //% block="get school %school | %energyType usage"
+    //% shim=energy::querySchoolEnergyFor
+    export function querySchoolEnergyFor(school: string, energyType: EnergyType): number {
         return Math.floor(Math.random() * 25) + 100;
     }
 }
